98ec92e281a8ed167dfcfb296342975639177d0b
fengao <fengao1004@126.com>
2017-07-06 11:00:41 +0800

    更新了上传的lib

--- app/src/main/java/com/dayang/sdkdemo/MainActivity.java
+++ app/src/main/java/com/dayang/sdkdemo/MainActivity.java
@@ -1,16 +1,11 @@
 package com.dayang.sdkdemo;
 
+import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
 
 import com.dayang.common.CMToolsManager;
-import com.quanshi.sdk.BaseResp;
-import com.quanshi.sdk.ConferenceReq;
-import com.quanshi.sdk.TangCallback;
-import com.quanshi.sdk.TangInterface;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,14 +18,20 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //http://clue.dayang.com/ClueCloudMobile?index=xs&FormType=DAY       token是678370dbd00e9a85c70996076956dfb
-                CMToolsManager.getInstance().openWebApp(MainActivity.this, "http://yun.csztv.com/baoliao/index.html?FormType=DAY&teamToken=db056a966f56ef6a2598333adeada40&appId=7d85849a6fdc7e585f67058c30ee0c09");
+                CMToolsManager.getInstance().openWebApp(MainActivity.this, "http://yun.csztv.com/baoliao/index.html?FormType=DAY&teamToken=2af500630616c68e8d7b36a4eafc3be&appId=7d85849a6fdc7e585f67058c30ee0c09");
             }
         });
         findViewById(R.id.login).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                CMToolsManager.getInstance().joinLiveConnection(getApplicationContext(),"DAY/Meeting");
+                CMToolsManager.getInstance().joinLiveConnection(getApplicationContext(), "DAY/Meeting");
             }
         });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        CMToolsManager.getInstance().destroy();
     }
 }
